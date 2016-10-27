2108ecc01e4b3d9f7333d556fbb9eae4c3aeae34
adson <adson@172.16.173.108>
2016-10-26 23:50:26 -0300

    update 26/10 23:50

--- src/br/stalkersat/fachada/Teste.java
+++ src/br/stalkersat/fachada/Teste.java
@@ -1,27 +1,17 @@
 package br.stalkersat.fachada;
 
 import br.stalkersat.bem.Bem;
-import br.stalkersat.bem.ControladorBem;
-import br.stalkersat.bem.RepositorioBemJDBC;
 
 public class Teste {
 	public static void main (String[] args){
 		
-		ControladorBem controlador = new ControladorBem();
+		Fachada f = Fachada.getInstance();
+		
+		Bem bem = new Bem(2, "adsa123kjj32", "pct1080", f.procurarTipoBem(1), f.procurarUsuario(2));
+		
+		
+		
 	
-		Fachada fachada = Fachada.getInstance();
-		
-		RepositorioBemJDBC r = new RepositorioBemJDBC();
-		
-		Bem bem = new Bem();
-		bem.setIdBem(0);
-		bem.setChassi("c2221");
-		bem.setPlaca("plc1080");
-		bem.setUsuario(fachada.procurarUsuario(1));
-		bem.setTipoBem(fachada.procurarTipoBem(1));
-		
-		fachada.atualizarBem(bem);
-		
 		
 		
 	}
